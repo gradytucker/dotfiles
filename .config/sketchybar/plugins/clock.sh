f7c44e4243f5da82f86df18cedcc300ee2f30d8d
gradytucker <gradytucker@icloud.com>
2026-01-29 18:33:31 +1100

    remove junk

--- .config/sketchybar/plugins/clock.sh
+++
@@ -1,3 +0,0 @@
-sketchybar --set $NAME \
-  label="$(date '+%a %b %d %-I:%M %p')" \
-  icon="◷" icon.color=0xffff9cbe
--- .config/sketchybar/plugins/portfolio.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#!/Users/gradytucker/.pyenv/shims/python
-python_output=$(python ~/.config/sketchybar/plugins/csmb.py 2>&1)
-
-sketchybar --set portfolio label="$python_output" 
